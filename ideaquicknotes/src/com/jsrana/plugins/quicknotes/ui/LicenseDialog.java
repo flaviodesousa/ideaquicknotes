d0307b8674a5afc7ae4f8cc82223fc116af7928b
jsrana <jsrana@gmail.com>
2010-02-20 16:17:47 +0000

    Version 2.6

---
+++ ideaquicknotes/src/com/jsrana/plugins/quicknotes/ui/LicenseDialog.java
@@ -0,0 +1,62 @@
+package com.jsrana.plugins.quicknotes.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class LicenseDialog
+        extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JTextArea textArea1;
+
+    public LicenseDialog() {
+        setContentPane( contentPane );
+        setTitle( "License" );
+        setModal( true );
+        getRootPane().setDefaultButton( buttonOK );
+
+        buttonOK.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {onOK();}
+        } );
+
+        setDefaultCloseOperation( DO_NOTHING_ON_CLOSE );
+        addWindowListener( new WindowAdapter() {
+            public void windowClosing( WindowEvent e ) {
+                onCancel();
+            }
+        } );
+
+        contentPane.registerKeyboardAction( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke( KeyEvent.VK_ESCAPE, 0 ), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT );
+
+        textArea1.setText( " Licensed under the Apache License, Version 2.0 (the \"License\")\n" +
+                           " you may not use this file except in compliance with the License.\n" +
+                           " You may obtain a copy of the License at\n" +
+                           " \n" +
+                           "      http://www.apache.org/licenses/LICENSE-2.0\n" +
+                           " \n" +
+                           " Unless required by applicable law or agreed to in writing, software\n" +
+                           " distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                           " WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                           " See the License for the specific language governing permissions and\n" +
+                           " limitations under the License.\n" );
+        textArea1.setMargin( new Insets( 2, 2, 2, 2 ) );
+        textArea1.setBackground( Color.WHITE );
+    }
+
+    private void onOK() {
+        dispose();
+    }
+
+    private void onCancel() {
+        dispose();
+    }
+}
