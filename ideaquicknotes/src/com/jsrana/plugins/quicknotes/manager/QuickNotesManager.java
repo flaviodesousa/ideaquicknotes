af40664ec603247ca08af7764177ed37bee654c8
jsrana <jsrana@gmail.com>
2010-04-06 20:43:34 +0000

    reset devmode

--- ideaquicknotes/src/com/jsrana/plugins/quicknotes/manager/QuickNotesManager.java
+++ ideaquicknotes/src/com/jsrana/plugins/quicknotes/manager/QuickNotesManager.java
@@ -38,7 +38,7 @@ public class QuickNotesManager {
     private HashMap<String, QuickNotesPanel> panelMap;
     private int index = 0;
     private static QuickNotesManager instance = new QuickNotesManager();
-    public static boolean devmode = true;
+    public static boolean devmode = false;
 
     private boolean showLineNumbers = true;
     private Font notesFont = new Font( "Arial", Font.PLAIN, 12 );
